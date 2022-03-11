875e73cd00c2d3fe134f7325426e35b3fe40226c
selezenart <artvs14@gmail.com>
2022-03-11 23:31:15 +0200

    dockerfile added, gunicorn deployment addded and heroku file added

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'very-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data', 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1) or 'sqlite:///' + os.path.join(basedir, 'data', 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
